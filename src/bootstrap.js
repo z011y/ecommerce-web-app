f8ccd2f27906854ac0d8e0b78e2d58c0f7fa9842
cameron-charles <cameron.ntc@gmail.com>
2020-01-16 16:19:32 -0700

    build base for the shop component. built shop route

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -20,6 +20,7 @@ import Layout from "./components/layout";
 import SignIn from "./components/auth/signin";
 import SignUp from "./components/auth/signup";
 import Account from "./components/account/account";
+import Shop from "./components/shop/shop";
 
 function main() {
   ReactDOM.render(
@@ -31,6 +32,7 @@ function main() {
             <Route path="/signin" exact component={SignIn} />
             <Route path="/signup" exact component={SignUp} />
             <Route path="/account" exact component={Account} />
+            <Route path="/shop" exact component={Shop} />
           </Switch>
         </Layout>
       </Router>
---
+++ src/components/shop/shop.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+class Shop extends Component {
+  componentDidMount() {
+    const headerLinks = [
+      {
+        _id: 0,
+        title: "Shop",
+        path: "/shop"
+      },
+      {
+        _id: 1,
+        title: "Logout",
+        path: "/"
+      }
+    ];
+    this.props.setHeaderLinks(headerLinks);
+
+    // fetch shop products action creator
+    // fetch navbar links
+    // set the navbar links
+  }
+  render() {
+    return (
+      <div className="shop">
+        {/* shop search bar  */}
+        {/* shop product */}
+        {/* shop cart button */}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { state };
+}
+
+Shop = connect(
+  mapStateToProps,
+  actions
+)(Shop);
+
+export default Shop;
