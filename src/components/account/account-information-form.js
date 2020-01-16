4cb684f0eb43b9dcb7a68c86e59d791b4c404aaf
cameron-charles <cameron.ntc@gmail.com>
2020-01-16 16:05:33 -0700

    finished account information form

--- src/components/account/account-information-form.js
+++ src/components/account/account-information-form.js
@@ -9,6 +9,10 @@ import history from "../../history";
 class AccountInformationForm extends Component {
   constructor() {
     super();
+
+    this.state = {
+      showPasswords: false
+    };
   }
 
   render() {
@@ -69,31 +73,68 @@ class AccountInformationForm extends Component {
           name="zipcode"
         />
 
-        {/* <Field
-          className="account-information-form__password"
-          component={FormInput}
-          type="password"
-          title="Password"
-          placeholder="Password"
-          name="password"
-        />
-        <Field
-          className="account-information-form__confirm"
-          component={FormInput}
-          type="password"
-          title="Confirm Password"
-          placeholder="Confirm Password"
-          name="confirm"
-        />
-        <div className="account-information-form__line"></div> */}
-        <Field
-          className="account-information-form__change-password"
-          component={FormButton}
-          onClick={() => console.log("tryna show passwords")}
-          type="button"
-          title="Change Password"
-          name="change-password"
-        />
+        {this.state.showPasswords ? (
+          [
+            <Field
+              key={0}
+              className="account-information-form__current"
+              component={FormInput}
+              type="password"
+              title="Current Password"
+              placeholder="Current Password"
+              name="current"
+            />,
+            <Field
+              key={1}
+              className="account-information-form__new"
+              component={FormInput}
+              type="password"
+              title="New Password "
+              placeholder="New Password"
+              name="new"
+            />,
+            <Field
+              key={2}
+              className="account-information-form__confirm"
+              component={FormInput}
+              type="password"
+              title="Confirm Password"
+              placeholder="Confirm Password"
+              name="confirm"
+            />,
+            <div key={3} className="account-information-form__line"></div>,
+            <Field
+              key={4}
+              className="account-information-form__update-information"
+              component={FormButton}
+              onClick={() => this.setState({ showPasswords: false })}
+              type="submit"
+              title="Update Information"
+              name="update-information"
+            />,
+            <Field
+              key={5}
+              className="account-information-form__cancel"
+              component={FormButton}
+              onClick={() => this.setState({ showPasswords: false })}
+              type="button"
+              title="Cancel"
+              name="cancel"
+              short={true}
+            />
+          ]
+        ) : (
+          <Field
+            className="account-information-form__change-password"
+            component={FormButton}
+            onClick={() => this.setState({ showPasswords: true })}
+            type="button"
+            title="Change Password"
+            name="change-password"
+          />
+        )}
+
+        <div className="account-information-form__line"></div>
       </form>
     );
   }
