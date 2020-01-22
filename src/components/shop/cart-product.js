77a0fa50f9111435502cb88c5cab48035f101483
cameron-charles <cameron.ntc@gmail.com>
2020-01-22 12:35:48 -0700

    built the shop cart product component

---
+++ src/components/shop/cart-product.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Quantity from "../quantity";
+import PurplePriceTag from "../purple-price-tag";
+
+class CartProduct extends Component {
+  render() {
+    const { className, title, quantity, price } = this.props;
+    return (
+      <div className={`${className} cart-product`}>
+        <img
+          className="cart-product__image"
+          src="../../../static/assets/poncho.jpg"
+        />
+        <div className="cart-product__title">{title}</div>
+        <Quantity className="cart-product__quantity" quantity={quantity} />
+        <a className="cart-product__remove">Remove</a>
+        <PurplePriceTag className="cart-product__price" title={price} />
+      </div>
+    );
+  }
+}
+
+export default CartProduct;
--- src/components/shop/shop-cart.js
+++ src/components/shop/shop-cart.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import CartProduct from "./cart-product";
+
 function CartButton({ className, icon }) {
   return (
     <div className={`${className} cart-button`}>
@@ -12,7 +14,7 @@ function CartButton({ className, icon }) {
 
 function CartContent({ className, products }) {
   let count = products.length;
-  let productsJSX = products.map(product => <h1 key={product}>{product}</h1>);
+  let productsJSX = products.map(product => <CartProduct key={product} />);
   return (
     <div className={`${className} cart-content`}>
       <div className="cart-content__title">Cart ({count})</div>
