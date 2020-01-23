746bb8d26a6d0c4566a508a978b88d4ea4a55643
cameron-charles <cameron.ntc@gmail.com>
2020-01-22 23:17:57 -0700

    built review form buttons

---
+++ src/components/order/review-form.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import { reduxForm, Field } from "redux-form";
+
+import { FormButton } from "../form-fields";
+
+import history from "../../history";
+
+class ReviewForm extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { className, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit} className={`${className} review-form`}>
+        <div className="review-form__line"></div>
+        <Field
+          className="review-form__proceed"
+          component={FormButton}
+          onClick={() => history.push("/account")}
+          type="submit"
+          title="Procced to Checkout"
+          name="proceed"
+        />
+        <Field
+          className="review-form__back"
+          component={FormButton}
+          onClick={() => history.push("/shop")}
+          type="button"
+          title="Back"
+          name="back"
+          short={true}
+        />
+      </form>
+    );
+  }
+}
+
+ReviewForm = reduxForm({
+  form: "ReviewForm"
+})(ReviewForm);
+
+export default ReviewForm;
--- src/components/order/review.js
+++ src/components/order/review.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import * as actions from "../../actions";
 
 import PageTitle from "../page-title";
+import ReviewForm from "./review-form";
 
 class Review extends Component {
   componentDidMount() {
@@ -11,10 +12,15 @@ class Review extends Component {
     this.props.setNavbarLinks([]);
   }
 
+  onSubmit = fields => {
+    console.log("fields");
+  };
+
   render() {
     return (
       <div className="review">
         <PageTitle className="review__page-title" title="Order Review" />
+        <ReviewForm className="review__form" onSubmit={this.onSubmit} />
       </div>
     );
   }
